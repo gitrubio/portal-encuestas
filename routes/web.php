05d3080bed6118ba5cc2a515c8146cc1caeca0c1
camosqueraba <camosqueraba@gmail.com>
2022-01-14 09:08:56 -0500

    modificaciones en las opciones en los formularios de validacion de usuario

--- routes/web.php
+++ routes/web.php
@@ -48,8 +48,9 @@ Route::get('/',[EncuestaController::class,'index'])->name('preguntas.index');
 Route::post('respuestas', [respuestasController::class, 'store'])
             ->name('respuestas.store');
    
-          Route::get('/', function () {
-                return view('form-encuesta.queja');*/
+        /*  Route::get('/', function () {
+                return view('form-encuesta.queja');
+          });*/
 /*Route::get('/', function () {
 
     return view('encuesta.inicioqueja');
