6b529e8ffe819117f1e6f4a4f49bdf34d939b324
emenavid <emavi0723@gmail.com>
2022-01-18 12:09:40 -0500

    editando el diseño de la dashboard

---
+++ resources/views/dashboard/crear_pregunta.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Crear preguntas</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <button class="btn btn-button" type="button">crear pregunta</button>
+    </div>
+    
+</body>
+</html>
