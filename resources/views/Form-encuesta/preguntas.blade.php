6a9ca3b9505fd0950e09aa09f77537eb2ef2cae8
emenavid <emavi0723@gmail.com>
2022-02-17 08:29:34 -0500

    intentando los step

--- resources/views/Form-encuesta/preguntas.blade.php
+++ resources/views/Form-encuesta/preguntas.blade.php
@@ -5,10 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href=" {{ asset('css/app.css') }}" rel="stylesheet">
-
-   
-
+    <link href=" {{ asset('css/appprogress.css') }}" rel="stylesheet">
     <script type="text/javascript" src=" {{asset('js/app.js') }}" ></script>
+    {{--<script type="text/javascript" src=" {{asset('js/appprogress.js') }}" ></script>
+
+    <link href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' rel='stylesheet'>
+    <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet'>
+    <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>--}}
+
     <title>Encuesta Perfect Body Medical Center</title>
 </head>
 <body>
@@ -95,6 +99,17 @@
             @php
               $contador++;
             @endphp
+{{-- ********************************************************************************************+ --}}
+{{-- ********************************************************************************************+ --}}
+{{-- ********************************************************************************************+ --}}
+
+      
+{{-- ********************************************************************************************+ --}}
+{{-- ********************************************************************************************+ --}}
+{{-- ********************************************************************************************+ --}}
+
+
+
             <fieldset>           
              <p class="text-center"><strong>{{$pregunta->pregunta}}</strong></p>
               <div class="">
@@ -133,3 +148,6 @@
   <script src="{{asset('assets/js/core/popper.min.js')}}"></script>
   <script src="{{asset('assets/js/core/bootstrap.min.js')}}"></script>
   <script src="{{asset('assets/js/plugins/perfect-scrollbar.jquery.min.js')}}"></script>
+
+  <script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js'></script>
+  
